9a4c1104b17c36404da668bc75ea67fc65013dc6
kongman551 <eggy551@gmail.com>
2022-03-15 11:51:47 -0400

    adds AddTask function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
       text: 'Doctor',
       day: 'Feb 5th at 2:30pm',
-      reminder: true,
+      reminder: false,
     },
     {
       id: 2,
       text: 'School',
       day: 'Feb 6th at 1:30pm',
-      reminder: true,
+      reminder: false,
     },
     {
       id: 3,
@@ -24,6 +26,13 @@ const App = () => {
       reminder: false,
     },
   ])
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   //Delete Task
   const deleteTask = (id) => {
@@ -41,7 +50,8 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
