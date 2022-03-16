2b034c8f59b19a3d63c601fb3e4686177468a95d
kongman551 <eggy551@gmail.com>
2022-03-16 15:53:14 -0400

    First Commit to Heroku

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
-import { useParams, Navigate, useNavigate } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
+import Button from "./Button"
 
 function TaskDetails() {
     const [loading, setLoading] = useState(true)
@@ -30,6 +31,9 @@ function TaskDetails() {
         <div>
             <h3>{task.text}</h3>
             <p>{task.day}</p>
+            <Button onClick={() => {
+                navigate(-1)
+            }} text='Go Back' />
         </div>
     )
 }
